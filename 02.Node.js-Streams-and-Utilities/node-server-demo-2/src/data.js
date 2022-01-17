0745ca40e6f8e1d89603fc870e554d2ba230836a
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-01-17 19:06:02 +0200

    Added data.js file to node-server-demo-2

---
+++ 02.Node.js-Streams-and-Utilities/node-server-demo-2/src/data.js
@@ -0,0 +1,42 @@
+const fs = require('fs').promises;
+
+async function readFile() {
+    const data = JSON.parse((await fs.readFile('./data/products.json')).toString());
+    return data;
+}
+
+async function getProducts() {
+    const data = await readFile();
+    return Object
+        .entries(data)
+        .map(([id, item]) => Object.assign({}, item, { _id: id }));
+}
+
+async function getProductById(id) {
+    const data = await readFile();
+    return data[id];
+}
+
+async function createProduct(product) {
+    const data = await readFile();
+    const _id = nextId();
+    data[_id] = product;
+    await fs.writeFile('./data/products.json', JSON.stringify(data, null, 2));
+}
+
+async function updateProduct(id, product) {
+    const data = await readFile();
+    data[id] = product;
+    await fs.writeFile('./data/products.json', JSON.stringify(data, null, 2));
+}
+
+function nextId() {
+    return 'xxxxxxxx'.replace(/x/g, () => (Math.random() * 16 | 0).toString());
+}
+
+module.exports = {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct
+}
