c1f60352760cae7dcc55a662bc21fd500d494643
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-09 23:41:42 +0200

    Hash password in user model.

--- 08.Workshop-Session-and-Authentication/Cubicle-Workshop-Part-3/models/User.js
+++ 08.Workshop-Session-and-Authentication/Cubicle-Workshop-Part-3/models/User.js
@@ -1,9 +1,18 @@
 const { Schema, model, Types: { ObjectId } } = require('mongoose');
+// const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: { type: String, required: true, minlength: [3, 'Username cannot be with less than 3 characters.'] },
     password: { type: String, required: true },
 });
+
+/* hash password in the model before saving in database
+userSchema.pre('save', function (next) {
+    console.log(this);
+    this.password = await bcrypt.hash(this.password, 10);
+    return;
+})
+*/
 
 const User = model('User', userSchema);
 
